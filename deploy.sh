41eac1a14d93c68ab4f957d2d6642f8db6c1200d
Alexis Miguel Jimenez Olivares <alexis.m.jimenez.o@gmail.com>
2022-09-29 01:05:39 -0500

    feat: add script to deploy to gh pages

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo >.nojekyll
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github-mickambar19.com:mickambar19/workspace-tool.git main:gh-pages
+
+cd -
